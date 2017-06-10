266f6aa99ede6668032fabe35b771d67b3957985
Jean Marguerite <jean.marguerite@outlook.com>
2017-06-10 18:56:41 +0200

    Start Cozmo actions implementation

--- affordable/affordable.py
+++ affordable/affordable.py
@@ -19,6 +19,29 @@ You should have received a copy of the GNU General Public License
 along with Affordable.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import time
+from affordable import cozmo_actions, interactions
+from cozmo.objects import LightCube1Id
+import cozmo
+
+
+def affordable_program(robot: cozmo.robot.Robot):
+    cube1 = robot.world.get_light_cube(LightCube1Id)
+
+    if cube1 is not None:
+        cube1.set_lights(cozmo.lights.green_light)
+
+    cozmo_robot = cozmo_actions.CozmoActions(robot, cube1)
+    cozmo_robot.move(interactions.Interaction.Type.FORWARD)
+    cozmo_robot.move(interactions.Interaction.Type.TURN_LEFT)
+    cozmo_robot.move(interactions.Interaction.Type.FORWARD)
+    cozmo_robot.move(interactions.Interaction.Type.EAT)
+    cozmo_robot.move(interactions.Interaction.Type.CHARGE)
+    cozmo_robot.move(interactions.Interaction.Type.TURN_RIGHT)
+    cozmo_robot.move(interactions.Interaction.Type.FORWARD)
+    cozmo_robot.move(interactions.Interaction.Type.MISS)
+    time.sleep(3)
+
 
 def main():
-    print("Affordable")
+    cozmo.run_program(affordable_program)
